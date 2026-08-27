c2586ffbeb9cc52be416cad8b28580b7691112e6
YourName <your-email@example.com>
2026-08-27 15:12:52 -0700

    new update

--- tools/youtube.py
+++ tools/youtube.py
@@ -20,14 +20,22 @@ def download_youtube_video(video_url):
     print("EXISTS:", os.path.exists(COOKIE_FILE))
 
     ydl_opts = {
-        "format": "best",
+        "format": "bestvideo*+bestaudio*/best",
 
         "outtmpl": "downloads/%(title)s.%(ext)s",
+
+        "merge_output_format": "mp4",
 
         "cookiefile": COOKIE_FILE,
 
         "js_runtimes": {
             "node": {}
+        },
+
+        "extractor_args": {
+            "youtube": {
+                "player_client": ["web"]
+            }
         },
 
         "postprocessors": [
@@ -37,7 +45,7 @@ def download_youtube_video(video_url):
             }
         ]
     }
-    
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 
         info = ydl.extract_info(
