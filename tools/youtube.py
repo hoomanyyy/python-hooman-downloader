53a48b681ea1cf1470cfaf517490205e7a7dd8e4
YourName <your-email@example.com>
2026-08-27 14:56:34 -0700

    asdas

--- tools/youtube.py
+++ tools/youtube.py
@@ -20,7 +20,7 @@ def download_youtube_video(video_url):
     print("EXISTS:", os.path.exists(COOKIE_FILE))
 
     ydl_opts = {
-        "format": "bestvideo+bestaudio/best",
+        "format": "best[ext=mp4]/best",
 
         "outtmpl": "downloads/%(title)s.%(ext)s",
 
@@ -32,12 +32,6 @@ def download_youtube_video(video_url):
             "node": {}
         },
 
-        "extractor_args": {
-            "youtube": {
-                "player_client": ["android"]
-            }
-        },
-
         "postprocessors": [
             {
                 "key": "FFmpegVideoConvertor",
@@ -45,7 +39,6 @@ def download_youtube_video(video_url):
             }
         ]
     }
-
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 
         info = ydl.extract_info(
